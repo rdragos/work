62023fd0434888735cdb20fe958d07445ed48cf2
Rotaru Dragos Alin <r.dragos0@gmail.com>
2013-05-26 16:20:17 +0300

    fixed test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,25 @@
 #include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <cstring>
+#include <string>
+#include <set>
 
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define ll long long
 
 using namespace std;
 
 int main() {
+	ifstream cin("test.in");
+	ofstream cout("test.out");
+
 	return 0;
 }
