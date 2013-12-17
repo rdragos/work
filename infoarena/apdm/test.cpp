e953830abe032f2644ee8c82e7e039c4a3453330
Rotaru Dragos Alin <r.dragos0@gmail.com>
2013-12-18 01:39:00 +0200

    apmd-100

--- infoarena/apdm/test.cpp
+++ infoarena/apdm/test.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <set>
 #include <stack>
+
 #define pb push_back
 
 #define mp make_pair
@@ -66,30 +67,44 @@ int main() {
     ifstream cin("apdm.in");
     ofstream cout("apdm.out");
     
+    srand(time(NULL));
 
     int N; cin >> N;
     vector <vector <int> > edges(N, vector<int>());
-
     int M; cin >> M;
     
+    vector <pair <int, int> > P(M);
     for (int i = 0; i < M; ++i) {
         int x, y;
         cin >> x >> y;
         x -= 1; y -= 1;
         edges[x].push_back(y);
         edges[y].push_back(x);
+        P[i] = make_pair(x, y);
     }
     
+    random_shuffle(P.begin(), P.end());
 
     int answer = N;
 
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N + min(300, M); ++i) {
         queue <int> Q; 
         vector <int> visited(N, 0);
         vector <vector <int> > graph(N, vector <int>());
-
-        Q.push(i);
-        visited[i] = 1;
+        
+        
+        if (i >= N) {
+            int x = P[i - N].first;
+            int y = P[i - N].second;
+            
+            graph[x].push_back(y);
+            graph[y].push_back(x);
+            Q.push(x); Q.push(y);
+            visited[x] = visited[y] = 1;
+        } else {
+            Q.push(i);
+            visited[i] = 1;
+        }
         
         while(!Q.empty()) {
             int x = Q.front(); Q.pop();
