806bc4cf25b9d420949864e3f806eff92c9ca389
Rotaru Dragos Alin <r.dragos0@gmail.com>
2013-12-22 18:57:08 +0200

    added lfa project

---
+++ teme/lfa-regular-expression/automat.h
@@ -0,0 +1,134 @@
+
+#ifndef AUTOMAT_H
+#define AUTOMAT_H
+
+class nod_automat {
+  public:
+	int f_state ;
+	vector <nod_automat*> neigh[27];	
+	nod_automat(int x = 0) : f_state(x) {}
+
+} ;
+
+
+class automat {
+  private:
+	  nod_automat *T;
+	  vector <nod_automat*> end_states;
+	
+	  const int LAMBDA;
+	  int convert(char x) {
+		  return int(x - 'a');
+	  }
+
+	  void insert(nod_automat *t, string s , int i) {
+		  if (s.size() == i) {
+				t -> f_state = 1;
+				end_states.push_back(t);
+				return ;
+		  }
+		  int f = convert(s[i]);	
+		  t -> neigh[f].push_back(new nod_automat()); 
+		  insert(t -> neigh[f][ t -> neigh[f].size() - 1], s, i + 1);	  	 
+	  }
+	  vector <nod_automat*> get_end_states() {
+		  return end_states;
+	  }
+	  nod_automat *get_begin() {
+		  return T;
+	  }
+  public:
+	  automat(string S) : LAMBDA(26) {
+		  T = new nod_automat();
+		  insert(T, S, 0);
+	  }		  
+	  int See(string S) {
+			
+		queue < pair <int, nod_automat*> > Q;
+		Q.push(make_pair(0, T));				
+		
+		map <pair <int, nod_automat *> , int> mark;
+		while (!Q.empty()) {			  
+			pair <int, nod_automat*> A = Q.front() ; 
+			Q.pop();
+					
+			if (A.second -> f_state && A.first == S.size())
+				return 1;
+			
+			if (A.first == S.size()) {	
+//			cerr << S[A.first] << " ";
+				
+				for (int i = 0; i < A.second -> neigh[LAMBDA].size(); ++i) {
+					if (mark[make_pair(A.first, A.second -> neigh[LAMBDA][i])]) {
+						continue;
+					}
+					Q.push( make_pair (A.first, A.second -> neigh[LAMBDA][i]) );
+					mark[make_pair(A.first, A.second -> neigh[LAMBDA][i])] = 1;
+				}
+			}
+			else {
+
+				for (int i = 0 ; i < A.second -> neigh[ convert(S[A.first]) ].size(); ++i) {
+					if (mark[make_pair(A.first + 1,A.second -> neigh[ convert(S[A.first])][i])])
+						continue;
+					Q.push( make_pair (A.first + 1, A.second -> neigh[ convert(S[A.first]) ][i]));
+					mark[ make_pair( A.first + 1, A.second -> neigh [convert (S[A.first])][i] ) ] = 1;
+				}
+				for (int i = 0; i < A.second -> neigh[LAMBDA].size(); ++i) {
+					if (mark[ make_pair( A.first, A.second -> neigh[LAMBDA][i])]) {
+						continue;
+					}
+					Q.push( make_pair(A.first, A.second -> neigh[LAMBDA][i]));
+					mark[ make_pair( A.first, A.second -> neigh[LAMBDA][i])] = 1;
+				}
+			}
+		}
+		return 0;	
+	  }	
+
+// concat <=> (A1 + A2) <=> A1A2
+	  void concat(automat &Y) {	
+		  nod_automat *A2 = Y.get_begin();
+
+		  for (int j = 0; j < end_states.size(); ++j) {
+			  end_states[j] -> f_state = 0;	
+			  end_states[j] -> neigh[LAMBDA].push_back(A2);
+		  }  
+		  end_states = Y.get_end_states();
+	  }
+// A1 | A2
+	  void unite(automat &Y) {
+		  nod_automat *A2 = Y.get_begin();
+		  
+		  T -> neigh[LAMBDA].push_back(A2);
+		  vector <nod_automat*> tmp = Y.get_end_states();
+
+		  for (int i = 0; i < tmp.size(); ++i)
+			  end_states.push_back(tmp[i]);	
+	  }
+//	A*
+	  void star() {
+		  for (int i = 0; i < end_states.size(); ++i) {
+			  T -> neigh[LAMBDA].push_back(end_states[i]);
+			  end_states[i] -> neigh[LAMBDA].push_back(T);
+		  }
+		  end_states.push_back(T);
+		  T -> f_state = 1;
+	  }
+
+};
+#endif
+/*
+int main() {
+	automat A("a"), B("b"), C("c");
+	A.concat(B); A.unite(C);
+	A.star();
+
+	while(1) {
+		string str;
+		cin >> str;
+		cout << A.See(str) << "\n";
+	}
+	return 0;
+}
+*/
---
+++ teme/lfa-regular-expression/main.cpp
@@ -0,0 +1,161 @@
+
+#include <cstdio>
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+#include <cstring>
+#include <string>
+#include <set>
+
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define ll long long
+
+using namespace std;
+
+#include "automat.h"
+
+
+class node_regex {
+	
+	private:
+		automat val;
+		char op;
+		node_regex* l;
+		node_regex*	r;
+		int mark;
+
+	public:
+		node_regex(string a = "", char b = 0 ,  node_regex* c = NULL, node_regex* d = NULL) :
+			val(a) , op(b) , l(c), r(d), mark(0) {
+		}
+		node_regex* getleft()  const {
+			return l;
+		}
+		node_regex* getright() const {
+			return r;
+		}
+		automat getval() const {
+			return val;
+		}
+		char getop() const {
+			return op;
+		}
+		void regex_mark_on() {
+			mark = 1;
+		}
+		int get_mark() {
+			return mark;
+		}
+
+};
+
+class Parse {
+	node_regex* Tree;
+	char *A;	
+	const int LMAX;
+	char *OP[4];
+	
+	private:
+		node_regex* get(int lev) {
+			node_regex *x, *y;
+
+			if (lev == LMAX) {
+				if (*A == '(') {
+					A++;
+					x = get(0);
+					A++;
+				} else {
+					string val = "";
+					for (; *A <= 'z' && *A >= 'a' ; ++A)
+						val += *A;
+					x = new node_regex(val);
+	//				cerr << x -> getval() << " " ;
+				}
+			} else {
+
+				for (x = get(lev + 1); strchr(OP[lev], *A); ) {
+					if (*A == '*') {
+						x -> regex_mark_on();
+						++A;
+						continue;
+					}
+					y = new node_regex("", *A++, x, get(lev + 1));
+					x = y;
+				}
+
+			}
+
+			return x;
+		}
+
+		automat eval(node_regex *x) const {
+			char op = x -> getop();
+			
+			
+			if(op == 0) {
+				automat L = x -> getval();
+				if (x -> get_mark()) {
+					L.star();
+				}
+				return L;
+			}
+
+			automat L = eval(x -> getleft());
+			automat R = eval(x -> getright());
+			
+			if (op == '+') {
+				L.concat(R);
+			}
+			if (op == '|') {
+				L.unite(R);
+			}
+			
+			if (x -> get_mark())
+				L.star();
+			return L;
+		}	
+
+
+	public:
+		Parse(char *B) : A(B), LMAX(3) {
+			OP[0] = "+";
+			OP[1] = "|";
+			OP[2] = "*";
+		}
+		automat get_things_done() {
+			Tree = get(0);		
+			return eval(Tree);
+		}
+
+
+};	
+int main() {
+	
+	
+	char *P = new char[1000];
+    cout << "Rules a* -> a^(n) n >= 0\n(a+b) -> (ab), (a|b) -> a or b\n";
+    cout << "Prints - You're the Boss -  if the word gave was matched to te regular expression you gave first time\n";
+
+    cout << "Give me a valid regular expresion:\n";
+	fgets(P, 1000, stdin);
+    Parse T(P);
+	
+	automat A = T.get_things_done();
+	string str;
+	
+	while(cin >> str) {
+		if(A.See(str)) {
+			cout << "You're the Boss\n";
+		} else {
+			cout << "Try again\n";
+		}
+	}
+
+	return 0;
+}
